c8e7884bd268dbcb2327e9b6837f4680992bd58d
Arun3sh <m.aruneshwar@gmail.com>
2022-08-23 22:36:30 +0530

    general update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Adduser from "./page/Adduser";
 
 function App() {
-  return <div className="App">View Employee</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          Home
+        </Route>
+        <Route path="/add-employee">
+          <Adduser />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
