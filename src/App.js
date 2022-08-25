dc3ae7927a56a21a6996ca0c0ef22d14953193cf
Arun3sh <m.aruneshwar@gmail.com>
2022-08-25 17:26:49 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Adduser from "./page/Adduser";
+import Editemployee from "./page/Editemployee";
+import Home from "./page/Home";
+import Viewemployee from "./page/Viewemployee";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          Home
+          <Home />
         </Route>
         <Route path="/add-employee">
           <Adduser />
+        </Route>
+        <Route path="/edit-employee/:id">
+          <Editemployee />
+        </Route>
+        <Route path="/view-employee/:id">
+          <Viewemployee />
         </Route>
       </Switch>
     </div>
